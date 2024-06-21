cb7f73d59ec5ed63b18d3e5602a3f9218245d285
Calvin Costa <calvinmoreiracosta@gmail.com>
2024-06-21 09:07:00 -0300

    Bug fix

--- views/tierlist_views.py
+++ views/tierlist_views.py
@@ -35,8 +35,8 @@ class TierlistView(View):
       if self.current_page == 1:
          self.first_page_button.disabled = True
          self.previous_page_button.disabled = True
-         self.next_page_button.disabled = True
-         self.last_page_button.disabled = True
+         self.next_page_button.disabled = False
+         self.last_page_button.disabled = False
       if self.current_page > 1:
          self.first_page_button.disabled = False
          self.previous_page_button.disabled = False
